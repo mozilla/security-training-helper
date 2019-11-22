86562d2376911ebc5c9aee5f96157c1b9bcc5f68
Cag <culucenk@mozilla.com>
2019-11-22 17:14:12 +1100

    Add the script

---
+++ src/post_process.py
@@ -0,0 +1,109 @@
+import csv
+import os
+import sys
+import argparse
+import logging
+import time
+
+DATA_FOLDER = os.getcwd() + "/../data"
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def read_csv(file):
+    # Read CSV file and return a list of contents as OrderedDict
+    csv_reader = csv.DictReader(file)
+    contents = []
+    for odict in csv_reader:
+        contents.append(odict)
+    return contents
+
+def add_missing_columns(departures):
+    for item in departures:
+        item.update({'New Hire': 'N'})
+        item.update({'Archive': 'ARCHIVE'})
+    return departures
+
+def add_new_hire_column(active, new):
+    temp_list = []
+    for item in active:
+        email = item['Email Address']
+        item.update({'New Hire': 'N'})
+        for new_hire in new:
+            if email == new_hire['Email Address']:
+                item.update({'New Hire': 'Y'})
+        temp_list.append(item)
+    return temp_list
+
+def combine(full, departures):
+    combined = full + departures
+    return combined
+
+def remove_duplicates(combined, departures):
+    final = combined.copy()
+    for item in final:
+        for departed in departures:
+            if 'Archive' in item.keys():
+                if item['Email Address'] == departed['Email Address'] and item['Archive'] == '':
+                    # This means we have a duplicate item
+                    combined.remove(item)
+            else:
+                item.update({'Archive': ''})
+    return combined
+
+def write_csv(contents, output):
+    field_names = [
+            "First Name",
+            "Last Name",
+            "Email Address",
+            "Cost Center",
+            "Hire Date",
+            "Employee Type",
+            "New Hire",
+            "Archive"
+    ]
+    writer = csv.DictWriter(output, fieldnames=field_names)
+    writer.writeheader()
+    for item in contents:
+        writer.writerow(item)
+
+def main(file1=None, file2=None, file3=None, outfile=None):
+    logging.info("Reading CSV files...")
+    active_employees = read_csv(file1)
+    new_hires = read_csv(file2)
+    departures = read_csv(file3)
+    time.sleep(2)
+
+    # STEP 1: Compare/combine active employees vs. new hires
+    logging.info("Adding missing columns to active employees...")
+    temp_active_employees = add_new_hire_column(active_employees, new_hires)
+    time.sleep(1)
+    # STEP 2: Prepare departures list (add the missing columns)
+    logging.info("Adding missing columns to departed employees...")
+    temp_departures = add_missing_columns(departures)
+    time.sleep(1)
+    # STEP 3: Compare/combine active employees with departured ones
+    logging.info("Collating all data into a single data set...")
+    combined_list = combine(temp_active_employees, temp_departures)
+    time.sleep(1)
+    # STEP 4: Prepare the final list with all additional columns
+    logging.info("Removing duplicates and finalising the data set...")
+    final_list = remove_duplicates(combined_list,temp_departures)
+    time.sleep(1)
+    # STEP 5: Write the contents to a new CSV file.
+    logging.info("Outputting the final data set...")
+    write_csv(final_list, outfile)
+    time.sleep(1)
+    logging.info("All done.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Optional argument
+    parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    required_arg_group = parser.add_argument_group('required arguments')
+    required_arg_group.add_argument('-a', '--all-employees', nargs=1, type=argparse.FileType('r', encoding='utf-8-sig'), required=True)
+    required_arg_group.add_argument('-n', '--new-hires', nargs=1, type=argparse.FileType('r', encoding='utf-8-sig'), required=True)
+    required_arg_group.add_argument('-t', '--terminated', nargs=1, type=argparse.FileType('r', encoding='utf-8-sig'), required=True)
+
+    args = parser.parse_args()
+    main(*args.all_employees, *args.new_hires, *args.terminated, args.outfile)
